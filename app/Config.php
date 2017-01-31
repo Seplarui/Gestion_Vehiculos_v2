3ce66940c14fadb3f122cbebb1423b1f46643191
Sebas Sebas <seplarui@gmail.com>
2017-01-31 21:56:16 +0100

    Config.php

--- app/Config.php
+++ app/Config.php
@@ -1,1 +1,12 @@
 <?php
+
+class Config {
+	
+	static public $bdhostname="localhost";
+	static public $bdnamebd="seplarui";
+	static public $bduser="alumno";
+	static public $bspass="alumno";
+	static public $file_css="estilo.css";
+	
+	
+}
