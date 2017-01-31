1cfe84da6c019e7fd6cc3f1abc8000d6b0553630
Sebas Sebas <seplarui@gmail.com>
2017-02-01 00:01:07 +0100

    Model.php

--- Pruebas/Conectar.php
+++ Pruebas/Conectar.php
@@ -1,20 +1,54 @@
 <?php
 
 include_once '../app/Config.php';
+include_once '../app/Model.php';
 
-$conexion = new PDO('mysql:host='.Config::$bdhostname.';dbname='.Config::$bdnamebd.';charset=utf8',Config::$bduser,Config::$bspass);
+//$conexion = new PDO('mysql:host='.Config::$bdhostname.';dbname='.Config::$bdnamebd.';charset=utf8',Config::$bduser,Config::$bspass);
 
-echo "Conexión correcta<br/>";
+//echo "Conexión correcta<br/>";
 
-$consulta = "select * from marcas";
+/*$consulta = "select * from marcas";
 
-$resultado = $conexion->query ( $consulta );
+$resultado = $conexion->query ( $consulta );*/
 
-foreach ($resultado as $fila) {
+/*foreach ($resultado as $fila) {
 	echo $fila['id']." ";
 	echo $fila['marca']."<br/>";
+}*/
+
+
+//LISTAR DESDE MODEL.PHP
+
+$modelo=new Model(Config::$bdhostname,Config::$bduser,Config::$bdpass,Config::$bdnamebd );
+
+echo "<br/>";
+print_r(Config::$bdhostname);
+echo "<br/>";
+print_r(Config::$bdnamebd);
+echo "<br/>";
+print_r(Config::$bduser);
+echo "<br/>";
+print_r(Config::$bdpass);
+
+$kk=$modelo->dameMarcas();
+
+//print_r($kk);
+
+foreach($kk as $fila) {
+	//echo $fila['id']." ";
+	//echo $fila['marca']."<br/>";
+	echo "<br/>";
+	echo $fila['id:Marca:private'];
+	echo "<br/>";
+	
 }
 
+/*$parametros=array($modelo->dameMarcas());
+foreach ($parametros as $fila) {
+	//print_r($fila);
+	print_r( $fila[3]);
+}
 
-
+print_r($parametros);
+*/
 ?>
--- app/Config.php
+++ app/Config.php
@@ -5,7 +5,7 @@ class Config {
 	static public $bdhostname="localhost";
 	static public $bdnamebd="seplarui";
 	static public $bduser="alumno";
-	static public $bspass="alumno";
+	static public $bdpass="alumno";
 	static public $file_css="estilo.css";
 	
 	
