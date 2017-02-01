2fb9f00eebcaaf1a6c3162d76f0be11cc40e6f6b
Sebas Sebas <seplarui@gmail.com>
2017-02-01 22:54:12 +0100

    Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -1,9 +1,59 @@
 <?php
-
 class Controller {
 	public function listar() {
-		$modelo=new Model(Config::$bdnombre,Config::$bdusuario,Config::$bdclave,Config::$bdhostname);
+		$modelo = new Model ( Config::$bdnombre, Config::$bdusuario, Config::$bdclave, Config::$bdhostname );
 		
-		$parametros
+		$parametros = array (
+				'mensaje' => 'Gestión de Vehículos',
+				'fecha' => date ( 'd-m-y' ) 
+		);
+	}
+	public function listar() {
+		$modelo = new Model ( Config::$bdnombre, Config::$bduser, Config::$bdclave, Config::$bdhostname );
+		
+		$parametros = array (
+				'marcas' => $model->dameMarca () 
+		);
+		
+		require __DIR__ . '/templates/mostrarMarcas.php';
+	}
+	public function insertar() {
+		$parametros = array (
+				'marca' => '' 
+		);
+		
+		$modelo = new Model ( Config::$bdnombre, Config::$bduser, Config::$bdclave, Config::$bdhostname );
+		
+		if (isset ( $_POST ['insertar'] )) {
+			
+			if($modelo->validarDatos($_POST['id'], $_POST['marca'])) {
+				if($modelo->insertarMarca($_POST['id'], $_POST['marca'])) {
+					header('Location: index.php?ctl=listar');
+				} else {
+					$parametros=array(
+							'marca'=>$_POST['marca'],
+					);
+					$parametros['mensaje']="No se ha podido insertar la marca";
+				}
+			} else {
+				$parametros=array(
+						'marca'=>$_POST['marca'],
+				);
+				$parametros['mensaje']="Hay datos que no son correctos";
+				
+			}
+			
+		}
+			require __DIR__.'/templates/formInsertar.php';
+		
+	
+	}
+	
+	public function buscarPorNombre() {
+		
+	}
+	
+	public function ver() {
+		
 	}
 }
