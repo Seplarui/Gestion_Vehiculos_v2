0d9616e27d0e98e3a9babed659ea06356925f043
Sebas Sebas <seplarui@gmail.com>
2017-02-03 18:34:37 +0100

    Completar Model.php

--- app/Marca.php
+++ app/Marca.php
@@ -3,10 +3,14 @@ class Marca {
 	private $marca;
 	private $modelo;
 	private $motor;
-	public function __construct($marca, $modelo, $motor) {
+	public function __construct($id, $marca, $modelo, $motor) {
+		$this->id = $id;
 		$this->marca = $marca;
 		$this->modelo = $modelo;
 		$this->motor = $motor;
+	}
+	function getId() {
+		return $this->id;
 	}
 	function getMarca() {
 		return $this->marca;
--- app/Model.php
+++ app/Model.php
@@ -1,1 +1,52 @@
 <?php
+include_once 'Marca.php';
+class Model {
+	protected $conexion;
+	public function __construct($dbname, $dbuser, $dbpass, $dbhost) {
+		$this->conexion = NULL;
+		try {
+			$bdconexion = new PDO ( "mysql:host=" . $dbhost . ";dbname=" . $dbname . ";charset=utf8", $dbuser, $dbpass );
+			
+			$this->conexion = $bdconexion;
+		} catch ( PDOException $ex ) {
+			echo "Error: " . $ex->getMessage ();
+		}
+	}
+	
+	// LISTADO MARCAS
+	public function dameMarcas() {
+		$consulta = "select * from marca";
+		$resultado = $this->conexion > query ( $consulta );
+		$marcas = array ();
+		$cont = 0;
+		
+		$filas = $resultado->fetchAll ( PDO::FETCH_OBJ );
+		
+		foreach ($filas as $fila) {
+			$marca= new Marca($fila->id, $fila->marca,$fila->modelo,$fila->motor);
+			$marcas[$cont]=$marca;
+			$cont++;
+		}
+		
+		$conexion=NULL;
+		return $marcas;
+	}
+	
+	// BUSCAR MARCAS POR NOMBRE
+	
+	public function buscarMarcaPorMarca($marca) {
+		$marca=htmlspecialchars($marca);
+		$consulta= "select * from marca where marca like".$marca;
+		$resultado=$this->conexion->query($consulta);
+		$marcas=array();
+		$cont=0;
+		$filas=$resultado->fetchAll(PDO::FETCH_OBJ);
+		foreach($filas as $fila) {
+			$marca= new Marca($fila->id,$fila->marca,$fila->modelo,$fila->motor);
+			$marcas...[POR AQUÍ]
+		}
+	}
+	
+	
+}
+
