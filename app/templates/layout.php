88f8f78510931c90ea164936f8b4948a295c225e
Sebas Sebas <seplarui@gmail.com>
2017-02-04 11:46:44 +0100

    Layout.php

---
+++ app/templates/layout.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>Gestión de Vehículos</title>
+<meta charset="utf8">
+<link rel="stylesheet" type="text/css"
+	href="<?php echo 'css/'.Config::$css?>" />
+</head>
+
+<body>
+
+	<div id="cabecera">
+
+		<h1>Gestión de Vehículos</h1>
+	</div>
+
+	<div id="menu">
+
+		<hr>
+
+		<a href="index.php?ctl=inicio">Inicio</a> <a
+			href="index.php?ctl=listar">Listar Vehículos</a> <a
+			href="index.php?ctl=insertar">Insertar Vehículos</a> <a
+			href="index.php?ctl=buscar">Buscar por Marca</a> <a
+			href="index.php?ctl=buscarPorId">Buscar por "Id"</a>
+
+		<hr>
+	</div>
+
+	<div id="contenido">
+	
+	<?php echo $contenido?>
+	
+	
+	</div>
+
+	<div id="pie">
+
+		<hr>
+		<div align="center">Pie de Página</div>
+
+	</div>
+
+
+</body>
+
+</html>
