97a20f75796c71e5f59b946e097335d65948de65
Sebas Sebas <seplarui@gmail.com>
2017-02-04 17:53:34 +0100

    verMarca.php

---
+++ app/templates/verMarca.php
@@ -0,0 +1,27 @@
+<?php ob_start()?>
+
+<h1><?php echo $parametros['marca']?></h1>
+<table>
+
+	<tr>
+
+		<td>Marca</td>
+		<td><?php echo $marca['marca']?></td>
+
+	</tr>
+	<tr>
+		<td>Modelo</td>
+		<td><?php echo $marca['modelo']?></td>
+
+	</tr>
+	<tr>
+
+		<td>Motor</td>
+		<td><?php echo $marca['motor']?></td>
+	</tr>
+
+<?php $contenido=ob_get_clean()?>
+<?php include 'layout.php'?>
+
+
+</table>
