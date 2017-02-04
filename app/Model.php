03c6fc917c50e15d4460c7f45bde047be1201cda
Sebas Sebas <seplarui@gmail.com>
2017-02-04 11:46:25 +0100

    Model.php fin

--- app/Model.php
+++ app/Model.php
@@ -43,9 +43,62 @@ class Model {
 		$filas=$resultado->fetchAll(PDO::FETCH_OBJ);
 		foreach($filas as $fila) {
 			$marca= new Marca($fila->id,$fila->marca,$fila->modelo,$fila->motor);
-			$marcas...[POR AQUÍ]
+			$marcas[$cont]=$marca;
+			$cont++;
+		}
+		$conexion=false;
+		return $marcas;
+	}
+	
+	public function buscarMarcaporId($id) {
+		$consulta="select * from marca where id=:id;";
+		$resultado=$this->conexion->prepare($consulta);
+		$resultado->execute(array(":id"=>id));
+		$marcas=array();
+		$cont=0;
+		
+		$fila=$resultado->fetchAll(PDO::FETCH_OBJ);
+		$marca= new Marca($fila->id,$fila->marca,$fila->modelo,$fila->motor);
+		$conexion=false;
+		return $marca;
+		
+	}
+	
+	public function insertarMarca($marca,$modelo,$motor) {
+		try {
+			$consulta="insert into marca(marca,modelo,motor) values ($marca,$modelo,$motor)";
+			
+			$resultado=$this->conexion->prepare($consulta);
+			
+			if(!$resultado) {
+				echo "Error al insertar.";
+			}
+			
+			$cont=$resultado->execute(array(":marca"=>$marca,
+					":modelo"=>$modelo,
+					":motor"=>$motor,
+					
+			));
+		} catch (PDOException $ex) {
+			echo "Error: ".$ex->getMessage();
+			return false;
+		}
+		$conexion=false;
+		if($cont==1) {
+			return true;
+		} else {
+			return false;
 		}
 	}
+		
+		public function validarDatos($marca,$modelo,$motor) {
+			$valido= is_string($marca) & is_string($modelo) & is_string($motor);
+		}
+	}
+	
+	?>
+	
+	
 	
 	
 }
